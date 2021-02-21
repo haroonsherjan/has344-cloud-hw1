6ad6e56e400733d4bce3315334584d8bd5d05817
Haroon <haroonsherjan@gmail.com>
2021-02-21 12:18:45 -0500

    Update api sdk

--- assets/js/sdk/apigClient.js
+++ assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://1n9gs7gtd3.execute-api.us-east-1.amazonaws.com/dev';
+    var invokeUrl = 'https://0o6ahg3uhl.execute-api.us-east-1.amazonaws.com/dev';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
