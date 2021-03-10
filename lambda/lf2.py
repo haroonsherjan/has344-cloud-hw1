55dada570d7aaa11f840ebfa8b8e683b90859f2f
Haroon <haroonsherjan@gmail.com>
2021-03-10 15:39:52 -0500

    Send multiple restaurants

--- lambda/lf2.py
+++ lambda/lf2.py
@@ -4,25 +4,24 @@ import json
 import boto3
 import random
 
-
-def getRestaurantIdGivenCuisine(recommendationRequest):
-    host = 'search-elastic-yelp-final-hunkmkikl3kafz5esv3cu6v5tu.us-east-1.es.amazonaws.com'  # For example, my-test-domain.us-east-1.es.amazonaws.com
-    region = 'us-east-1'  # e.g. us-west-1
+def getRestaurantIdsGivenCuisine(recommendationRequest):
+    host = 'search-elastic-yelp-final-hunkmkikl3kafz5esv3cu6v5tu.us-east-1.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
+    region = 'us-east-1' # e.g. us-west-1
 
     service = 'es'
     credentials = boto3.Session().get_credentials()
     awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
     es = Elasticsearch(
-        hosts=[{'host': host, 'port': 443}],
-        http_auth=awsauth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
+        hosts = [{'host': host, 'port': 443}],
+        http_auth = awsauth,
+        use_ssl = True,
+        verify_certs = True,
+        connection_class = RequestsHttpConnection
     )
     searchBody = {
         "query": {
-            "bool": {
+            "bool":{
                 "must": [{
                     "match": {
                         "cuisine": recommendationRequest['Cuisine'].lower()
@@ -39,29 +38,34 @@ def getRestaurantIdGivenCuisine(recommendationRequest):
     print(response)
     total = response['hits']['total']['value']
     hits = response['hits']['hits']
-    return random.choice(hits)['_id']
+    return {random.choice(hits)['_id'], random.choice(hits)['_id'], random.choice(hits)['_id'],
+            random.choice(hits)['_id'], random.choice(hits)['_id']}
 
 
-def getRestaurantGivenId(id):
+def getRestaurantsGivenIds(ids):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('yelp-restaurants')
-    return table.get_item(
-        Key={
-            'id': id
-        }
-    )
+    restaurants = []
+    for id in ids:
+        restaurants.append(table.get_item(
+            Key ={
+                'id': id
+            }
+        ))
+    return restaurants
 
 
-def sendTextMessage(restaurant, recommendationRequest):
+def sendTextMessage(restaurants, recommendationRequest):
     sns = boto3.client('sns')
+    message = 'Hello! Here are some {} restaurant suggestions for {} people, for '.format(recommendationRequest['Cuisine'].capitalize(), recommendationRequest['NumberOfPeople']) + '{} at {}: \n'.format(recommendationRequest['Date'], recommendationRequest['DiningTime'])
+    count = 0
+    for restaurant in restaurants:
+        count = count + 1;
+        message = message + "{}. {}, located at {}. ".format(count, restaurant['Item']['name'], restaurant['Item']['address'])
+    message = message +  "Enjoy your meal!"
     return sns.publish(
-        PhoneNumber="+1" + recommendationRequest['PhoneNumber'],
-        Message=
-        "Hello! Here is a {} restaurant suggestion for {} people, for ".format(
-            recommendationRequest['Cuisine'].capitalize(), recommendationRequest['NumberOfPeople']) +
-        "{} at {}: ".format(recommendationRequest['Date'], recommendationRequest['DiningTime']) +
-        "{}, located at {}. Enjoy your meal!".format(restaurant['Item']['name'], restaurant['Item']['address']
-                                                     )
+        PhoneNumber= "+1" + recommendationRequest['PhoneNumber'],
+        Message=message
     )
 
 
@@ -70,14 +74,10 @@ def lambda_handler(event, context):
     recommendationRequest = event['Records'][0]['body']
     if isinstance(recommendationRequest, str):
         recommendationRequest = json.loads(recommendationRequest)
-    id = getRestaurantIdGivenCuisine(recommendationRequest)
-    restaurant = getRestaurantGivenId(id)
-    response = sendTextMessage(restaurant, recommendationRequest)
-    # message = json.loads(event['Records'][0]['Sns']['Message'])
-    # words = message['messageBody']
-    # number = message['originationNumber']
+    ids = getRestaurantIdsGivenCuisine(recommendationRequest)
+    restaurants = getRestaurantsGivenIds(ids)
+    response = sendTextMessage(restaurants, recommendationRequest)
     return {
         'statusCode': 200,
         'body': response
-        # 'body': json.dumps("Phone number: {}\nMessage Text: {}".format(number, words))
     }
